c452d88d564450e0de4e2829ecc8bf4f0a1648a7
cybercoder <barqawi.88@outlook.com>
2024-02-04 11:19:45 +0000

    add intellicloud semantic search

---
+++ intelli/tests/integration/test_intellicloud_wrapper.py
@@ -0,0 +1,34 @@
+import unittest
+import os
+from wrappers.intellicloud_wrapper import IntellicloudWrapper
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class TestIntellicloudWrapper(unittest.TestCase):
+
+    def setUp(self):
+        """Initialize the wrapper with an API key."""
+        api_key = os.getenv("INTELLI_ONE_KEY")
+        # get the dev intelli url for the test case
+        api_base = os.getenv("INTELLI_API_BASE")
+        self.assertIsNotNone(api_key, "INTELLI_ONE_KEY must not be None.")
+        self.intellicloud = IntellicloudWrapper(api_key, api_base)
+    
+    def test_semantic_search(self):
+        query_text = "Why is Mars called the Red Planet?"
+        k = 2
+        result = self.intellicloud.semantic_search(query_text, k)
+        print('Semantic Search Result: ', result)
+        self.assertTrue(len(result) > 0, "Semantic search should return at least one result")
+    
+    def test_semantic_search_with_filter(self):
+        query_text = "Why is Mars called the Red Planet?"
+        k = 2
+        filters = {'document_name': 'test_mars_article.pdf'}
+        result = self.intellicloud.semantic_search(query_text, k, filters)
+        print('Semantic Search Result with Filter: ', result)
+        self.assertTrue(len(result) > 0, "Semantic search with filter should return at least one result")
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ intelli/wrappers/intellicloud_wrapper.py
@@ -0,0 +1,26 @@
+import requests
+from config import config  # Assuming you have a configuration dictionary similar to Node.js config.json
+from utils.conn_helper import ConnHelper
+
+class IntellicloudWrapper:
+    def __init__(self, api_key, api_base=None):
+        self.ONE_KEY = api_key
+        self.API_BASE_URL = api_base if api_base else config['url']['intellicloud']['base']
+
+    def semantic_search(self, query_text, k=3, filters=None):
+        if filters is None:
+            filters = {}
+            
+        url = f"{self.API_BASE_URL}{config['url']['intellicloud']['semantic_search']}"
+        # set the data
+        data = {'one_key': self.ONE_KEY, 'query_text': query_text, 'k': k}
+        if filters and 'document_name' in filters:
+            data['document_name'] = filters['document_name']
+        
+        # call the document search
+        try:
+            response = requests.post(url, data=data)
+            response.raise_for_status()
+            return response.json()['data']
+        except requests.RequestException as e:
+            raise Exception(ConnHelper.get_error_message(e))
