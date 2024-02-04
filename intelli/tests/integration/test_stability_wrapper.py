d1f8a1e0af6ac9cd16edbf31fa366ea298bb1acb
cybercoder <barqawi.88@outlook.com>
2024-02-04 12:29:47 +0000

    update the default model

--- intelli/tests/integration/test_stability_wrapper.py
+++ intelli/tests/integration/test_stability_wrapper.py
@@ -25,8 +25,8 @@ class TestStabilityAIWrapper(unittest.TestCase):
                 }
             ],
             "cfg_scale": 7,
-            "height": 512,
-            "width": 512,
+            "height": 1024,
+            "width": 1024,
             "samples": 1,
             "steps": 20
         }
