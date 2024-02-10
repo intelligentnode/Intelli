550efdc7a7f0ee54640ca220a80b8b247136b8c2
cybercoder <barqawi.88@outlook.com>
2024-02-10 22:46:04 +0000

    Update types.py

--- intelli/flow/types.py
+++ intelli/flow/types.py
@@ -4,8 +4,10 @@ from enum import Enum
 class AgentTypes(Enum):
     TEXT = 'text'
     IMAGE = 'image'
+    VISION = 'vision'
 
 
 class InputTypes(Enum):
     TEXT = 'text'
     IMAGE = 'image'
+    VISION = 'vision'
