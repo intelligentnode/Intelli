c9608ae83501aca93277bbf95fe852079448b6c2
cybercoder <barqawi.88@outlook.com>
2024-02-10 21:50:47 +0000

    adjust the requirements

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setup(
     packages=find_packages(),
     python_requires='>=3.6',
     install_requires=[
-        "requests-mock==1.11.0", "python-dotenv==1.0.1", "networkx==3.2.1"
+        "python-dotenv==1.0.1", "networkx==3.2.1"
     ],
 )
